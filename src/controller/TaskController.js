aec7cf30b4b6d47f0a0f88bd8fcb4cf533115b25
joaogaabriel <joaogabriel13ferreira@gmail.com>
2022-11-12 12:07:46 -0300

    impl metodo post, persitencia mongoDb

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,6 +1,31 @@
-const getAll = (req, resp) =>{
-    return resp.render("index")
-}
+const Task = require("../models/Task");
+
+const getAllTask = async (req, res) => {
+  try {
+    const tasksList = await Task.find();
+    return res.render("index", {tasksList});
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const createTask = async (req, res) => {
+  const task = req.body;
+
+  if (!task.task) {
+    return res.redirect("/");
+  }
+
+
+  try {
+    await Task.create(task);
+    return res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
-    getAll,
-}
+  getAllTask,
+  createTask,
+};
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const port = 3000;
 connectToDb();
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded())
 app.use(routes)
 
 
